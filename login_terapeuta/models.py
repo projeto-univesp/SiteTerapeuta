496c80374d5aa054a75d5604c6748ffa6d3f98db
LarissaPedroso <lary_santos2000@hotmail.com>
2024-04-05 19:21:41 -0300

    Model login criada

--- login_terapeuta/models.py
+++ login_terapeuta/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Terapeuta(models.Model):
+    idTerapeuta = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField()
+    senha = models.CharField(max_length=100)
